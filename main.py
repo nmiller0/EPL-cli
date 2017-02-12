884e9924f8530c116d5a8c6a2b78b152d5c523e5
Neal Miller <Neal.Miller@Outlook.com>
2017-02-11 22:19:45 -0500

    1617.csv

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
 import datetime
 import urllib.request
+import csv
 
 def main():
     print("EPL-cli \nEnter a command: ")
     date1 = datetime.date.today()
     global season
-    season = str((date1.year - 1)) + "-" + str((date1.year % 2000))
+    season = str((date1.year - 1)%2000) + str((date1.year % 2000))
     print(season)
     UsrInput = input()
     if UsrInput == "teams" or "Teams":        
             teams()
 
-    
-
 def download(str):
-    url = 'https://raw.githubusercontent.com/openfootball/eng-england/master/'+ str + '/1-premierleague.conf.txt'
-    urllib.request.urlretrieve(url, "teams.txt")
+    url = 'http://www.football-data.co.uk/mmz4281/' +season+ '/E0.csv'
+    urllib.request.urlretrieve(url, (season + ".csv"))
     
 def teams():
     download(season)
-    print("Current Teams: ")
-    infile = open("teams.txt", "r")
-    teams = infile.read()
-    print(teams)
+    infile = open(season + ".csv")
+    reader = csv.DictReader(infile)
+    teams = []
+    for row in reader:
+        teams.append(row['HomeTeam'])
+    i = 0
+    tempTeams = teams[:]
+    for x in teams:
+        for i in range(tempTeams.count(x)-1):
+            tempTeams.remove(x)
+    teams = tempTeams[:]
+    for x in teams:
+        print(x)
 
 main()
     
